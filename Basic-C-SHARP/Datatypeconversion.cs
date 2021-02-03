4fb634ba6333b22d4d76134f91a481f755a8e53d
SAMEERBALANAVAR <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-03 08:32:40 -0800

    Add files via upload

---
+++ Basic-C-SHARP/Datatypeconversion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/** 
+program on datatype conversion
+coder sameer
+date 3/feb/2021
+
+ * */
+namespace ConsoleApp1
+{
+    //class name Datatypeconveri
+    class Datatypeconversion
+    {
+        static void Main(string[] args)
+        {
+            //converting string to double
+            Console.WriteLine("Enter decimal number:");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Entered Number is: " + num);
+            Console.WriteLine("Enter String:");
+            string stringvalue = Console.ReadLine();
+
+            Console.WriteLine("Entered String is: " + stringvalue);
+
+        }
+    }
+}
