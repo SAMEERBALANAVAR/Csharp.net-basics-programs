ca8554a76d744fe711a0de2c267814f91d3514a5
SAMEERBALANAVAR <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-03 08:14:34 -0800

    Update Addition.cs

--- Basic-C-SHARP/Addition.cs
+++ Basic-C-SHARP/Addition.cs
@@ -4,16 +4,25 @@ using System.Text;
 
 namespace Basic_C_SHARP
 {
+    //class name Addtion
     class Addition
     {
         static void Main(string[] args)
         {
+            //Data type is int with variable are a,b,c
             int a, b, c;
+            //taking input from user for a var
             Console.WriteLine("Enter value for a");
+            //store that value inside a
             a = int.Parse(Console.ReadLine());
+             //taking input from user for b var
             Console.WriteLine("Enter value for b");
+             //store that value inside b
             b = int.Parse(Console.ReadLine());
+            //addition of a and b and are stored inside c var
             c = a + b;
+            
+           //output of the program
             Console.Write("Addition of a and b is = " + c);
 
 
