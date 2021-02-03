05a5c20023071c3aabdc036fdb3276ad66c4ca7d
SAMEERBALANAVAR <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-03 08:29:40 -0800

    Update Multiplication.cs

--- Basic-C-SHARP/Multiplication.cs
+++ Basic-C-SHARP/Multiplication.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+//program on Multiplication by sameer on 3/feb/2021
 namespace Basic_C_SHARP
 {
+    //class name Multiplication
     class Multiplication
     {
         static void Main(string[] args)
         {
+            //datatype is int type and  variables are a,b,c 
             int a, b, c;
+            //taking input from user a
             Console.WriteLine("Enter value for a");
+            //store value in a
             a = int.Parse(Console.ReadLine());
+            //taking input from user a
             Console.WriteLine("Enter value for b");
+            //store value in b
             b = int.Parse(Console.ReadLine());
+            //multiplication operation done below
             c = a * b;
+            //printing ouput using 3rd var ie c
             Console.Write("Multiplication of a and b is = " + c);
 
 
