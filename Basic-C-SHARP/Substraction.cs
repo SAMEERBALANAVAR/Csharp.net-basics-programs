a6ae6602e1b0927b568d6dae862e1605571acd09
SAMEERBALANAVAR <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-03 08:32:10 -0800

    Update Substraction.cs

--- Basic-C-SHARP/Substraction.cs
+++ Basic-C-SHARP/Substraction.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+//program on Substraction by sameer on 3/feb/2021
 namespace Basic_C_SHARP
 {
+    //class name Substraction
     class Substraction
     {
         static void Main(string[] args)
         {
+            //datatype is int type and var are a,b,c
             int a, b, c;
+            //taking input from user a
             Console.WriteLine("Enter value for a");
+            //storing values on to a
             a = int.Parse(Console.ReadLine());
+            ////taking input from user b
             Console.WriteLine("Enter value for b");
+            //storing values on to b
             b = int.Parse(Console.ReadLine());
+            //Substraction operation done below
             c = a - b;
+            //printing values using 3rd var ie c
             Console.Write("substraction of a and b is = " + c);
 
 
