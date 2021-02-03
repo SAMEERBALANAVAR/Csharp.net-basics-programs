ed600b9106e32a05bd8985066565179851da5eca
SAMEERBALANAVAR <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-03 08:20:12 -0800

    Update Decreament.cs
    comment section added

--- Basic-C-SHARP/Decreament.cs
+++ Basic-C-SHARP/Decreament.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+//program on Decreament operator by sameer on 3/feb/2021
 namespace Basic_C_SHARP
 {
+    //class name Decreament
     class Decreament
     {
         static void Main(string[] args)
         {
+            //datatype int value 
+            //x is var
             int x;
+            //taking input from users
             Console.WriteLine("Enter value for x \n");
+            //storing value into var x
             x = int.Parse(Console.ReadLine());
+            //postincreament procedure
             x++;
-            Console.Write("Preincreament of x is = " + x);
-            --x;
             Console.Write("Postincreament of x is = " + x);
+              //pretincreament procedure
+            --x;
+            Console.Write("Pretincreament of x is = " + x);
 
         }
     }
