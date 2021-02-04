faab88008431b234b9695f54394e0ee6e0c93273
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-04 02:03:00 -0800

    Add files via upload
    EnumerWithDifferent Actions

---
+++ Basic-C-SHARP/enumex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingSession2
+{
+    class enumex
+    {
+        enum Color { Blue, Black };
+
+
+
+        // Main Method 
+        public static void Main(String[] args)
+        {
+            Color c1 = Color.Blue;
+            Console.Write("TypeCode of Enum Constant " + c1 + " : ");
+
+
+
+            // Using the GetTypeCode() Method 
+            Console.WriteLine(c1.GetTypeCode());
+
+
+
+            Color c2 = Color.Blue;
+            Console.Write("HashCode of Enum Constant " + c2 + " : ");
+
+
+
+            // Using the GetTypeCode Method 
+            Console.WriteLine(c2.GetHashCode());
+        }
+
+    }
+}
+
