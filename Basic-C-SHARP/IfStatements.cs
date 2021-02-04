3076ad4496eaf3269346686286580e7c4fcd5303
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-04 02:54:30 -0800

    Update IfStatements.cs

--- Basic-C-SHARP/IfStatements.cs
+++ Basic-C-SHARP/IfStatements.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+//program on Condtion statements by sameer on 4/feb/2021
 namespace ConsoleApp1
 {
+    //class name IfStatements
     class IfStatements
     {
         static void Main(string[] args)
         {
+            //concidering total marks as 100
             int total = 100;
+            //taking values from users
             Console.WriteLine("enter the marks");
+            //storing values in total
             total = int.Parse(Console.ReadLine());
             if (total <= 35)
             {
