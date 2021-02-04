5652573e4bb09526842dd2a8e250548845592914
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-02-04 02:51:14 -0800

    Update Enumaration.cs

--- Basic-C-SHARP/Enumaration.cs
+++ Basic-C-SHARP/Enumaration.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+//program on Enumaration by sameer on 4/feb/2021
 namespace ConsoleApp1
 {
+    //class name Enumaration
     class Enumaration
     {
+        //Enum name Categories
         enum Categories
         {
+            //Different categories
             Electronics = 1,
             Food = 2,
             Automotive = 3,
@@ -18,10 +21,12 @@ namespace ConsoleApp1
         }
         public static void Main()
         {
+            //printing Electonics category
             Console.WriteLine(Categories.Electronics);
+            //print value of arts 
             int stream = (int)Categories.Arts;
             Console.WriteLine(stream);
-
+            //printing default value of category 11
             var wd = (Categories)11;
             Console.WriteLine(wd);
 
